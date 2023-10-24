f58848efa8d04ae65684131f663abdc1edc74c9e
sun0day <sun_day500@163.com>
2023-10-24 22:04:52 +0800

    test: node_modules

--- packages/core/__test__/index.spec.mjs
+++ packages/core/__test__/index.spec.mjs
@@ -6,7 +6,14 @@ const parser = new Parser()
 const parse = (file) => JSON.parse(parser.parse(Buffer.from(getCodeFile(file)), true).toString())
 
 test('should parse es', (t) => {
-  t.deepEqual(parse('es.js'), readParsedFile('es.json', ['es.js']))
+  t.deepEqual(
+    parse('es.js'), 
+    readParsedFile(
+      'es.json', 
+      ['es.js'],
+      {semver: 'semver/index.js'}
+    )
+  )
 })
 
 test('should parse ts', (t) => {
--- packages/core/__test__/utils.mjs
+++ packages/core/__test__/utils.mjs
@@ -5,13 +5,14 @@ import { fileURLToPath } from 'node:url'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const getCodeFile = (file) => join(__dirname, 'fixture/code', file)
-export const readParsedFile = (file, codeFiles = [], nodeModules = []) => {
+export const getNodeModules = (file) => join(__dirname, 'fixture/code/node_modules', file)
+export const readParsedFile = (file, codeFiles = [], nodeModules = {}) => {
   let content = readFileSync(join(__dirname, 'fixture/parsed', file)).toString()
   codeFiles.forEach((f) => {
     content = content.replaceAll(`{${f}}`, getCodeFile(f))
   })
-  nodeModules.forEach((f) => {
-    content = content.replaceAll(`{${f}}`, getCodeFile(f))
+  Object.keys(nodeModules).forEach((f) => {
+    content = content.replaceAll(`{${f}}`, getNodeModules(nodeModules[f]))
   })
   return JSON.parse(content)
 }
