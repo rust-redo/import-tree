7a227ae6e1145774f66df14ea3224ff88a3f0cfd
sun0day <sun_day500@163.com>
2023-11-13 21:16:21 +0800

    feat: core/index.ts export ts type

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,30 @@
 import {Parser as CoreParser} from './core'
 
+export enum ImportNodeType {
+  BUILTIN = 'builtin',
+  LOCAL = 'local',
+  NODE_MODULES = 'node_modules'
+}
+export interface ImportNode {
+  id: string
+  type: ImportNodeType
+  importer: string[] | null
+  import: ImportLink[] | null
+}
+
+export enum ImportLinkType {
+  STATIC = 'static',
+  DYNAMIC = 'dynamic',
+  REQUIRE = 'require'
+}
+
+export interface ImportLink {
+  id: string
+  type: ImportLinkType
+  ident: {name: string, as: string}[]
+}
+
+
 export class Parser {
   parser: CoreParser
   constructor({root}: {root?: string} = {}) {
@@ -15,7 +40,7 @@ export class Parser {
       depth?: number,
       resolve?: boolean
     } = {}
-    ) {
+  ): Record<string, ImportNode>  {
     return JSON.parse(this.parser.parse(Buffer.from(file), depth, resolve).toString())
   }
 }
